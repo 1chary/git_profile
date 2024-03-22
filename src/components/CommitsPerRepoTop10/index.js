8b45317621a8c7abefbfd066f4b1913dac2a11f7
1chary <147873086+1chary@users.noreply.github.com>
2024-03-22 21:30:30 +0530

    Update index.js

--- src/components/CommitsPerRepoTop10/index.js
+++ src/components/CommitsPerRepoTop10/index.js
@@ -1,4 +1,4 @@
-import {PieChart, Pie, Legend, Cell} from 'recharts'
+import {ResponsiveContainer, PieChart, Pie, Legend, Cell} from 'recharts'
 import './index.css'
 
 const COLORS = ['#54CA76', '#31A4E6', '#9261F3', '#F2637F', '#F5C452']
@@ -7,22 +7,24 @@ export default function CommitsPerRepoTop10(props) {
   const {repoCommitCountConvertedData} = props
 
   return (
-    <PieChart className="commitsPer10" width={600} height={400}>
-      <Pie
-        data={repoCommitCountConvertedData}
-        cx={200}
-        cy={200}
-        labelLine={false}
-        innerRadius={70}
-        outerRadius="outer"
-        fill="#8884d8"
-        dataKey="commits"
-      >
-        {repoCommitCountConvertedData.map((entry, index) => (
-          <Cell key="name" fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-      <Legend align="right" layout="vertical" verticalAlign="middle" />
-    </PieChart>
+    <ResponsiveContainer className="commitsPer10">
+        <PieChart>
+          <Pie
+            data={repoCommitCountConvertedData}
+            cx={200}
+            cy={200}
+            labelLine={false}
+            innerRadius={70}
+            outerRadius="outer"
+            fill="#8884d8"
+            dataKey="commits"
+          >
+            {repoCommitCountConvertedData.map((entry, index) => (
+              <Cell key="name" fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          <Legend align="right" layout="vertical" verticalAlign="middle" />
+        </PieChart>
+    </ResponsiveContainer
   )
 }
